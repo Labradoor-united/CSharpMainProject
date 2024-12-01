fe46bb083e16a3ee4496bb2f6425700c6cae554e
Labradoor-united <vadim.bash94@gmail.com>
2024-12-01 20:35:54 +0300

    HW-2

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -30,11 +30,16 @@ namespace Model.Runtime.Projectiles
             // Insert you code here
             ///////////////////////////////////////
 
+            float maxHeight = totalDistance * 0.6f;
+
+            float x = t * 2 - 1;
+            localHeight = maxHeight * (-x * x + 1);
+
 
             ///////////////////////////////////////
             // End of the code to insert
             ///////////////////////////////////////
-            
+
             Height = localHeight;
             if (time > StartTime + _timeToTarget)
                 Hit(_target);
